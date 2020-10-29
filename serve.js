8e6ee3d10d3c41c006c8b586bd0ff85e6425fb32
Jaylan Snelson <jaylan210@gmail.com>
2020-10-29 11:42:19 -0400

    getting first set of details added

--- serve.js
+++ serve.js
@@ -4,9 +4,10 @@ const puppeteer = require('puppeteer');
 let fs = require('fs');
 const fbiurl = "https://www.fbi.gov/wanted/kidnap";
 let missingpersons = [];
+let fbimissingpersondatafile = "fbimissingpersondata.txt"
 
 // function for geting intial links of missing persons from fbi url
-(async () => {
+let storeInitialFBIData = async () => {
     //const browser = await puppeteer.launch({headless: false});
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -22,8 +23,7 @@ let missingpersons = [];
 
     let persons = await page.evaluate(() => Array.from(document.querySelectorAll('.portal-type-person > .title'), element => element.textContent));
     let personslink = await page.evaluate(() => Array.from(document.querySelectorAll('.portal-type-person > .title a'), element => element.getAttribute('href')));
-      
-    
+    // creating object to write to a file which contains name of missing person and thier detials link
     persons.forEach((el, idx) =>  {
         missingpersons.push({
             name: removeNewLineCharactersFromPersonsName(el),
@@ -33,12 +33,48 @@ let missingpersons = [];
     function removeNewLineCharactersFromPersonsName(person) {
         return person.replace(/\n/g, "");
     }
-    //personslink.forEach(el =>  console.log(el));
 
-    fs.writeFile('missingpersondata.txt', JSON.stringify(missingpersons), err => {
+    fs.writeFile(fbimissingpersondatafile, JSON.stringify(missingpersons), err => {
         if(err) console.log(err);
         console.log("Saved.")
     })
   
     browser.close();
-  })();
+  };
+
+// function for geting details of each missing person from fbi missing person url
+  let getPersonsFromFBIData = async (link) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    let alldetails = [];
+  
+    await page.goto(link, { waitUntil: 'networkidle0' });
+
+    let details = await page.evaluate(() => Array.from(document.querySelectorAll('.wanted-person-details > p'), element => element.textContent));
+    details.forEach(el => alldetails.push({details: el}));
+    
+    console.log(alldetails);
+
+    browser.close();
+  };
+
+fs.readFile(fbimissingpersondatafile, async (err, data) => {
+    if(err) console.log(err);
+    let details = JSON.parse(data);
+    for(let i = 0; i <= details.length; i++) {
+        await getPersonsFromFBIData(details[i].link);
+    }
+})
+
+
+
+
+
+
+
+
+async function getalldata() {
+    await storeInitialFBIData();
+}
+
+//getalldata();
